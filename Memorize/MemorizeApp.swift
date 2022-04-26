ed6ab0139f507eb40164a828a0b2a5d204812338
Caio Oliveira <caiolv@me.com>
2022-04-25 21:30:06 -0300

    Start project

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Caio Oliveira on 25/04/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
